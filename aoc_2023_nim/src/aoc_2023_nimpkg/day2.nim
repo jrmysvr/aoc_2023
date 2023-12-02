8fa49921e8723145e68139cb1871e88f50b73c45
Jeremy Savor <jeremy.savor@gmail.com>
2023-12-02 10:55:48 +0100

    [nim] Solve day 2 part 1

--- aoc_2023_nim/src/aoc_2023_nimpkg/day2.nim
+++ aoc_2023_nim/src/aoc_2023_nimpkg/day2.nim
@@ -1,8 +1,24 @@
 import input
+import std/[enumerate, strutils, tables]
 
+const bag = {"red": 12, "green": 13, "blue": 14}.toTable
 
 proc solvePart1(input: string): string =
-  result = ""
+  var idSum = 0
+  for i, gameLine in enumerate(input.split('\n')):
+    let id = i + 1
+    let game = gameLine.strip.split(':')[1]
+    var possible = true
+    for grab in game.split(';'):
+      for blocks in grab.strip.split(','):
+        let countColor = blocks.strip.split(' ')
+        let (count, color) = (countColor[0], countColor[1])
+        if parseInt(count) > bag[color]:
+          possible = false
+          break
+    if possible:
+      idSum += id
+  result = $idSum
 
 
 proc solvePart2(input: string): string =
