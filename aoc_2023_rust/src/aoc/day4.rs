58b312760ffb1d888828add22cb11a36bdffc6b0
Jeremy Savor <jeremy.savor@gmail.com>
2023-12-04 15:35:27 +0100

    [rust] Finish day 4 parts 1 and 2

--- aoc_2023_rust/src/aoc/day4.rs
+++ aoc_2023_rust/src/aoc/day4.rs
@@ -1,8 +1,5 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-
 use crate::aoc::input::read_input_for_day;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 pub fn run() {
     println!("Day 4 Solutions");
@@ -17,11 +14,13 @@ pub fn run() {
 type Num = i32;
 type Wins = HashSet<Num>;
 type Nums = Vec<Num>;
-type Cards = Vec<(Wins, Nums)>;
+type CardId = usize;
+type Cards = Vec<(CardId, Wins, Nums)>;
+type CardResults = HashMap<CardId, usize>;
 
 fn convert_input_to_cards(input: &String) -> Cards {
     let mut cards = Cards::new();
-    for line in input.trim().split('\n') {
+    for (ix, line) in input.trim().split('\n').enumerate() {
         let all_numbers = line.split(':').nth(1).unwrap();
         let winning = all_numbers
             .split(' ')
@@ -36,34 +35,56 @@ fn convert_input_to_cards(input: &String) -> Cards {
             .skip(1)
             .map(|n| Num::from_str_radix(n, 10).unwrap())
             .collect::<Nums>();
-        cards.push((winning, numbers));
+        let card_id = ix + 1;
+        cards.push((card_id, winning, numbers));
     }
 
     cards
 }
 
-fn solve_part1(input: &String) -> String {
+fn convert_input_to_card_results(input: &String) -> CardResults {
     let cards = convert_input_to_cards(input);
-    let mut points = 0;
-    for (winning, numbers) in cards {
+    let mut results = CardResults::new();
+    for (card_id, winning, numbers) in cards {
         let count = numbers
             .iter()
             .filter(|num| winning.contains(num))
             .collect::<Vec<&Num>>()
             .len();
-        let point = if count > 0 {
+        results.entry(card_id).or_insert(count);
+    }
+
+    results
+}
+
+fn solve_part1(input: &String) -> String {
+    let card_results = convert_input_to_card_results(input);
+    let mut points = 0;
+    for (_, count) in card_results {
+        points += if count > 0 {
             2u32.pow((count as i32 - 1) as u32)
         } else {
             0
         };
-        points += point;
     }
 
     points.to_string()
 }
 
 fn solve_part2(input: &String) -> String {
-    String::new()
+    let card_results = convert_input_to_card_results(input);
+    let mut copies = vec![0; card_results.len()];
+    for card_id in 1..=card_results.len() {
+        let count = card_results[&card_id];
+        copies[card_id-1] += 1;
+        for _ in 0..copies[card_id-1] {
+            for i in 0..count {
+                copies[card_id+i] += 1;
+            }
+        }
+    }
+
+    copies.iter().fold(0, |acc, c| acc + c).to_string()
 }
 
 #[cfg(test)]
@@ -85,20 +106,25 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
     #[test]
     fn test_convert_input_to_cards() {
         let input = String::from("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
-        let expected_cards: Cards = vec![(Wins::from([41, 48, 83, 86, 17]), vec![])];
+        let expected_cards: Cards = vec![(1, Wins::from([41, 48, 83, 86, 17]), vec![])];
         let cards = convert_input_to_cards(&input);
 
         assert!(cards.len() == 1);
 
         for (actual, expected) in cards.iter().zip(expected_cards.iter()) {
-            let (a_win, _) = actual;
-            let (e_win, _) = expected;
+            let (_, a_win, _) = actual;
+            let (_, e_win, _) = expected;
             assert_eq!(a_win, e_win);
         }
     }
 
     #[test]
-    fn test_full() {
+    fn test_full_part1() {
         assert_eq!(solve_part1(&get_input(0)), "13");
+    }
+
+    #[test]
+    fn test_full_part2() {
+        assert_eq!(solve_part2(&get_input(0)), "30");
     }
 }
